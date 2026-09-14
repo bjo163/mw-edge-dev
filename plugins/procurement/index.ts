79caad4d1578ff39f4009c69ae2316d543366bfd
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-09-14 17:07:05 +0700

    feat(procurement): export purchase order lifecycle commands (#20)

--- plugins/procurement/index.ts
+++ plugins/procurement/index.ts
@@ -1,2 +1,10 @@
 import models from "./models.js";
 export { models };
+export {
+  approvePurchaseOrder,
+  cancelPurchaseOrder,
+  fulfillPurchaseOrder,
+  rejectPurchaseOrder,
+  submitPurchaseOrder,
+} from "./lifecycle.js";
+export type { ApprovalContext, PurchaseOrderStatus } from "./lifecycle.js";
