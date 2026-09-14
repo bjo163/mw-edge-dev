5a04bf782c49d1c0ff77fe5f4533334f396119b8
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-09-14 17:05:44 +0700

    feat(crm): export pipeline lifecycle commands (#18 #19)

--- addons/crm-pipeline/index.ts
+++ addons/crm-pipeline/index.ts
@@ -1,2 +1,9 @@
 import models from "./models.js";
 export { models };
+export {
+  advanceOpportunity,
+  convertLead,
+  disqualifyLead,
+  qualifyLead,
+} from "./lifecycle.js";
+export type { LeadStatus, OpportunityStage } from "./lifecycle.js";
